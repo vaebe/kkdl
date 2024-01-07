fb91b8675ec4f5408706cc8e5ce421388a4c6eec
zcj <18137693952@163.com>
2024-01-07 20:55:30 +0800

    feat(shortURL): 短链列表展示

--- src/api/shortURL.ts
+++ src/api/shortURL.ts
@@ -1,5 +1,6 @@
 import Api from './base';
-import type { ResultData } from './base';
+import type { ResultData, ResultPageListData } from './base';
+import type { PaginationParameter } from '@/types';
 
 export interface CreateParams {
   title: string;
@@ -15,3 +16,25 @@ export interface CreateResData {
 export const createShortURL = (
   data: CreateParams
 ): Promise<ResultData<CreateResData>> => Api.post('/shortURL/create', data);
+
+export interface GetShortURLListParams extends PaginationParameter {
+  title: string;
+  rawUrl: string;
+}
+
+export interface ShortURLInfo {
+  id: number;
+  shortUrl: string;
+  rawUrl: string;
+  expirationTime: string;
+  userId: string;
+  createdAt: string;
+  title: string;
+  groupId: 0;
+}
+
+// 获取短链列表
+export const getShortURLList = (
+  data: GetShortURLListParams
+): Promise<ResultPageListData<ShortURLInfo>> =>
+  Api.post('/shortURL/getList', data);
--- src/views/dataManagement/shortUrl/index.vue
+++ src/views/dataManagement/shortUrl/index.vue
@@ -1,7 +1,104 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import { getShortURLList } from '@/api/shortURL.ts';
+import { usePageList } from '@/composables';
+
+const { VITE_APP_BASE_URL } = import.meta.env;
+
+const searchForm = reactive({
+  title: '',
+  rawUrl: ''
+});
+
+const { reset, page, tableData, handleCurrentChange, removeRow } = usePageList({
+  searchForm,
+  getListApi: getShortURLList
+});
+reset();
+</script>
 
 <template>
-  <el-card>short-url</el-card>
+  <div>
+    <el-card class="mb-2">
+      <el-form :model="searchForm" label-width="90px">
+        <el-row :gutter="10">
+          <el-col :span="8">
+            <el-form-item label="短链标题：">
+              <el-input
+                v-model="searchForm.title"
+                placeholder="请输入短链标题"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="跳转链接：">
+              <el-input
+                v-model="searchForm.rawUrl"
+                placeholder="请输入跳转链接"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="8">
+            <el-row type="flex" justify="end">
+              <el-button @click="reset">重置</el-button>
+              <el-button type="primary" @click="handleCurrentChange(1)">
+                查询
+              </el-button>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-card>
+
+    <el-card>
+      <div class="flex items-center justify-between">
+        <span class="text-title">{{ $route.meta.title }}</span>
+        <el-button>新增</el-button>
+      </div>
+
+      <el-table :data="tableData" stripe style="width: 100%" class="my-2">
+        <el-table-column type="index" label="序号" width="90"></el-table-column>
+        <el-table-column label="短链标题" prop="title"></el-table-column>
+        <el-table-column label="短链" prop="shortUrl">
+          <template #default="scope">
+            {{ VITE_APP_BASE_URL?.replace('api', scope.row.shortUrl) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="跳转地址" prop="rawUrl"></el-table-column>
+        <el-table-column
+          label="创建时间"
+          prop="createdAt"
+          width="180"
+        ></el-table-column>
+        <el-table-column
+          label="过期时间"
+          prop="expirationTime"
+          width="180"
+        ></el-table-column>
+        <el-table-column label="操作" width="160" fixed="right">
+          <template #default="scope">
+            <el-button
+              type="danger"
+              link
+              @click="removeRow({ id: scope.row.id })"
+            >
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        background
+        hide-on-single-page
+        :current-page="page.pageNo"
+        :page-size="page.pageSize"
+        layout="total,prev, pager, next,jumper"
+        :total="page.total"
+        @current-change="handleCurrentChange"
+      ></el-pagination>
+    </el-card>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss" scoped></style>
