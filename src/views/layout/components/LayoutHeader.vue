774a4d913ce337c1d3b00c54b81f2031f0399003
vaebe <18137693952@163.com>
2024-05-17 21:36:53 +0800

    feat(layout): 移除 header 的菜单切换

--- src/views/layout/components/LayoutHeader.vue
+++ src/views/layout/components/LayoutHeader.vue
@@ -27,26 +27,6 @@ function jumpPage(path: string) {
       {{ VITE_APP_TITLE }}
     </h1>
 
-    <!--  用户登录后才需要展示菜单切换  -->
-    <ul v-if="isLogin" class="menu grid grid-cols-2 gap-2">
-      <li
-        class="cursor-pointer hover:text-blue-500" :class="[
-          { 'text-blue-600': $route.path.startsWith('/url') },
-        ]"
-        @click="jumpPage('url')"
-      >
-        创建短链
-      </li>
-      <li
-        class="cursor-pointer hover:text-blue-500" :class="[
-          { 'text-blue-600': $route.path.startsWith('/dm') },
-        ]"
-        @click="jumpPage('dm')"
-      >
-        数据管理
-      </li>
-    </ul>
-
     <div class="flex justify-end items-center">
       <el-popover v-if="isLogin" placement="bottom" trigger="click">
         <template #reference>
