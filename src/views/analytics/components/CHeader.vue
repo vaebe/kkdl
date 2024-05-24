980cfbdf17297351058c16ef7b9532a45b055e57
vaebe <18137693952@163.com>
2024-05-24 22:24:43 +0800

    feat(analytics): header 组件

---
+++ src/views/analytics/components/CHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const { VITE_APP_TITLE } = import.meta.env
+
+const router = useRouter()
+function jumpHome() {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div class="w-full h-[58px] px-20 flex items-center justify-between shadow">
+    <div class="w-1/2 h-full flex  items-center">
+      <img src="@/../public/logo.svg" alt="logo" style="width: 32px;height: 32px">
+      <h1 class="text-2xl text-black cursor-pointer ml-2" @click="jumpHome">
+        {{ VITE_APP_TITLE }}
+      </h1>
+    </div>
+
+    <div class="w-1/2 flex items-center justify-end cursor-pointer">
+      <router-link to="/register">
+        <p class="mr-4 text-gray-500 hover:text-black">
+          注册
+        </p>
+      </router-link>
+      <router-link to="/login">
+        <p class="px-6 py-0.5 bg-black border-4 border-white hover:border-4 hover:border-gray-200 text-white rounded-3xl ">
+          登录
+        </p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
