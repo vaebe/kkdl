e7579e875796f296da79fad833e67c54e59b6a05
vaebe <18137693952@163.com>
2024-05-24 22:06:37 +0800

    feat: 引入打包分析插件

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { compression } from 'vite-plugin-compression2'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,7 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     compression(),
+    visualizer(),
   ],
   optimizeDeps: {
     include: [
