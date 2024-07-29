72a5a40eb14bf9f91d5dce78250324f690fd0fae
ve <52314078+vaebe@users.noreply.github.com>
2024-07-29 10:12:29 +0800

    feat: 增加 loginContainer 组件

---
+++ src/components/loginContainer/CHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+const { VITE_APP_TITLE } = import.meta.env
+
+const router = useRouter()
+function jumpHome() {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div class="w-full h-[58px] px-4 shadow flex items-center justify-between">
+    <div class="w-full h-full flex items-center">
+      <img src="/logo.svg" alt="logo" style="width: 32px;height: 32px">
+      <h1 class="text-2xl text-black cursor-pointer ml-2" @click="jumpHome">
+        {{ VITE_APP_TITLE }}
+      </h1>
+    </div>
+
+    <router-link to="/analytics">
+      <Icon icon="tabler:chart-pie" class="cursor-pointer hover:text-blue-400" width="30px" height="30px" />
+    </router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/loginContainer/index.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup>
+const CHeader = defineAsyncComponent(() => import('./CHeader.vue'))
+const CFooter = defineAsyncComponent(() => import('./CFooter.vue'))
+
+const mode = useColorMode({
+  attribute: 'class',
+})
+mode.value = 'light'
+</script>
+
+<template>
+  <div class="login">
+    <div class="bg1" />
+    <div class="bg2" />
+    <div class="bg3" />
+
+    <CHeader class="absolute top-0 z-20" />
+
+    <div
+      class="absolute z-10 w-full h-full flex flex-col justify-center items-center"
+    >
+      <div
+        id="login-container"
+        class="p-6 rounded flex flex-col justify-center items-center"
+      >
+        <slot />
+      </div>
+    </div>
+    <CFooter class="absolute bottom-0 z-10" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background: #ffffff;
+  overflow: hidden;
+
+  .bg1 {
+    content: '';
+    background: url(https://assets.dub.co/misc/grid.svg) repeat;
+    z-index: 1;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    opacity: 0.4;
+    filter: invert(1);
+  }
+
+  .bg2 {
+    background: radial-gradient(circle, rgba(2, 0, 36, 0) 0, #fafafa 100%);
+    position: absolute;
+    content: '';
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+    top: 0;
+  }
+
+  .bg3 {
+    width: 100%;
+    max-width: 640px;
+    height: calc(100% - 80px);
+    position: absolute;
+    top: 80px;
+    z-index: 3;
+    filter: blur(100px) saturate(150%);
+    background-image: radial-gradient(
+        at 27% 37%,
+        hsla(215, 98%, 61%, 1) 0px,
+        transparent 0%
+      ),
+      radial-gradient(at 97% 21%, hsla(125, 98%, 72%, 1) 0px, transparent 50%),
+      radial-gradient(at 52% 99%, hsla(354, 98%, 61%, 1) 0px, transparent 50%),
+      radial-gradient(at 10% 29%, hsla(256, 96%, 67%, 1) 0px, transparent 50%),
+      radial-gradient(at 97% 96%, hsla(38, 60%, 74%, 1) 0px, transparent 50%),
+      radial-gradient(at 33% 50%, hsla(222, 67%, 73%, 1) 0px, transparent 50%),
+      radial-gradient(at 79% 53%, hsla(343, 68%, 79%, 1) 0px, transparent 50%);
+    opacity: 0.15;
+  }
+}
+</style>
