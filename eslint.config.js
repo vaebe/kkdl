106282933f18486b577e6344d2338f1191af1e87
vaebe <18137693952@163.com>
2025-06-10 20:29:13 +0800

    chore:  更新 eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,6 @@
-// eslint.config.js
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
-    css: true,
-  },
-  ignores: [
-    'node_modules/*',
-    '/dist',
-    '/src/*.d.ts',
-    '**.json',
-    '**.svg',
-    '*.js',
-  ]
+  formatters: true,
+  vue: true,
 })
